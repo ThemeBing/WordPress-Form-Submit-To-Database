c23061016c2eb7eae92a821d44e7ae1573772324
Farhan Khan <farhan01617@gmail.com>
2019-10-09 23:58:22 +0600

    Update form-page.php

--- form-page.php
+++ form-page.php
@@ -19,7 +19,6 @@ $sql = "CREATE TABLE $table_crm_form (
   id mediumint(9) NOT NULL AUTO_INCREMENT,
   value1 text NOT NULL,
   value2 text NOT NULL,
-  value3 text NOT NULL,
   PRIMARY KEY  (id)
 ) $charset_collate;";
 
@@ -31,36 +30,49 @@ dbDelta( $sql );
 // Insert data
 if ( isset( $_POST['form_submit'] ) ) {
 
-	var_dump($_POST);
+  global $wpdb;
 
-	global $wpdb;
-
-	$wpdb->insert( 
-		$table_crm_form, 
-		array(
-			'id' => '',
-			'value1' => $_POST['fullname'],
-			'value2' => $_POST['email'],
-			'value3' => $_POST['message'],
-		) 
-	);
+  $wpdb->insert( 
+    $table_crm_form, 
+    array(
+      'id' => '',
+      'value1' => $_POST['fullname'],
+      'value2' => $_POST['email']
+    ) 
+  );
 
 }
 
 // Get Data
-$get_data = $wpdb->get_var( "SELECT * FROM $table_crm_form",1,1 );
-echo $get_data;
+// $get_data = $wpdb->get_var( "SELECT * FROM $table_crm_form",1,1 );
+$get_data = $wpdb->get_results( "SELECT * FROM $table_crm_form" ); ?>
+<table>
+  <tbody>
+    <thead>
+      <tr>
+        <th scope="col">#</th>
+        <th scope="col">Name</th>
+        <th scope="col">Email</th>
+      </tr>
+    </thead>
+  <?php
+  $count = 1; foreach ( $get_data as $key => $value ) { ?>
+      <tr>
+        <th><?php echo $count++ ?></th>
+        <td><?php echo $value->value1 ?></td>
+        <td><?php echo $value->value2 ?></td>
+      </tr>
+  <?php } ?>
+  </tbody>
+</table>
 
-?>
 <div id="content">
     <form method="post">
         <label for="fullname">Full Name</label>
         <input type="text" name="fullname" id="fullname" required>
         <label for="email">Email Address</label>
         <input type="email" name="email" id="email" required>
-        <label for="message">Your Message</label>
-        <textarea name="message" id="message"></textarea>
-        <input type="submit" name="form_submit" value="Send My Message">
+        <input type="submit" name="form_submit">
     </form>
 </div>
 
